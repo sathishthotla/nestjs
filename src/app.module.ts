fab5ff10da16e3928bfba5cf39fa39cff0136b31
sathishthotla <sathishthotla11@gmail.com>
2023-04-20 21:22:21 +0530

    employee code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmployeeModule } from 'employee/employee.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Employee } from 'employee/entities/employee.entities';
 import { LaptopsModule } from './laptops/laptops.module';
 import { Laptop } from './laptops/entities/laptop.entity';
+import { EmployeeModule } from './employee/employee.module';
+import { Employee } from './employee/entities/employee.entities';
 
 @Module({
   imports: [EmployeeModule, TypeOrmModule.forRoot({
